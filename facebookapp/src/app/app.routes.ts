c3c82ea92fb86972adf1e38e67ad74b709b5ea7c
LCC pc <nexushellstein@gmail.com>
2025-11-12 08:58:37 +0800

    established routes

--- facebookapp/src/app/app.routes.ts
+++ facebookapp/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './features/login/login';
+import { Signup } from './features/signup/signup';
+import { Home } from './features/home/home';
+
+export const routes: Routes = [
+    { 
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    { 
+        path: 'login',
+        component: Login,
+    },
+    { 
+        path: 'signup',
+        component: Signup 
+    },
+    { 
+        path: 'home',
+        component: Home
+    },
+    {
+        path: '**',
+        redirectTo: 'login'
+    }
+];
+
