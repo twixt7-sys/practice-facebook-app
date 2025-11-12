4aba61ad4aac781a8093cd1252a100d15c742775
LCC pc <nexushellstein@gmail.com>
2025-11-12 09:03:13 +0800

    altered login

--- facebookapp/src/app/features/login/login.ts
+++ facebookapp/src/app/features/login/login.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './login.scss',
 })
 export class Login {
+  constructor(private router: Router) {
 
+  }
+
+  loginUser() {
+  this.router.navigate(['/home']);
+  }
+  signupUser() {
+
+  this.router.navigate(['/signup']);
+  }
 }
