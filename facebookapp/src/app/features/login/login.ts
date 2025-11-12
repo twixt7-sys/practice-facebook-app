24603ef24da1381a51505054f4ea96d8210113bc
LCC pc <nexushellstein@gmail.com>
2025-11-12 09:35:38 +0800

    established login. signup and auth guard

--- facebookapp/src/app/features/login/login.ts
+++ facebookapp/src/app/features/login/login.ts
@@ -1,22 +1,25 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
+
 export class Login {
   constructor(private router: Router) {
 
   }
 
   loginUser() {
-  this.router.navigate(['/home']);
+    this.router.navigate(['/home']);
   }
-  signupUser() {
 
-  this.router.navigate(['/signup']);
+  signupUser() {
+    this.router.navigate(['/signup']);
   }
 }
--- facebookapp/src/app/features/signup/signup.ts
+++ facebookapp/src/app/features/signup/signup.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signup.html',
   styleUrl: './signup.scss',
 })
 export class Signup {
+  constructor(private router: Router) {
 
+  }
+  registerUser() {
+    this.router.navigate(['/home'])
+  }
+  backToLogin() {
+    this.router.navigate(['/login'])
+  }
 }
