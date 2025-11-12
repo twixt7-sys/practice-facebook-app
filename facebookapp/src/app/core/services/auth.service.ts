80b89e35114f22aefb5c771a3cd6336d04864de6
LCC pc <nexushellstein@gmail.com>
2025-11-12 09:48:37 +0800

    established auth guard

---
+++ facebookapp/src/app/core/services/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl = 'https://your-server-url.com/'; // <-- replace with your backend URL
+
+  constructor(private http: HttpClient) {}
+
+  signup(data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}signup.php`, data);
+  }
+
+  login(data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}login.php`, data);
+  }
+
+  home(): Observable<any> {
+    return this.http.get(`${this.baseUrl}home.php`);
+  }
+}
